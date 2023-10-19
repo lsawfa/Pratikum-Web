3fead146d22092887d2a3b8a210bdf9540848d57
lsawfa <laryntsawfakenanga@gmail.com>
2023-10-19 07:39:02 +0700

    update script.js tugas 1 modul 2

--- Modul 2/Tugas/Tugas1/script.js
+++ Modul 2/Tugas/Tugas1/script.js
@@ -1,68 +1,62 @@
 const resultElement = document.querySelector('[name="result"]');
-const calculatorButtons = document.querySelectorAll('.cal-button');
+const calculatorButtons = document.querySelectorAll(".cal-button");
 
-let SecondInput = '';
-let calculateOperator = '';
-let FirstInput = '';
+let SecondInput = "";
+let calculateOperator = "";
+let FirstInput = "";
 
 function displayResult() {
   resultElement.textContent = SecondInput;
 }
 
 function handleInput(value) {
-  if (value === '=') {
+  if (value === "=") {
     if (SecondInput && FirstInput && calculateOperator) {
       switch (calculateOperator) {
-        case '+':
+        case "+":
           SecondInput = parseFloat(FirstInput) + parseFloat(SecondInput);
           break;
-        case '-':
+        case "-":
           SecondInput = parseFloat(FirstInput) - parseFloat(SecondInput);
           break;
-        case '*':
+        case "*":
           SecondInput = parseFloat(FirstInput) * parseFloat(SecondInput);
           break;
-        case '/':
+        case "/":
           SecondInput = parseFloat(FirstInput) / parseFloat(SecondInput);
           break;
-        case '^':
+        case "^":
           SecondInput = parseFloat(Math.pow(FirstInput, SecondInput));
           break;
-        case 'mod':
+        case "mod":
           SecondInput = parseFloat(FirstInput) % parseFloat(SecondInput);
           break;
       }
-      FirstInput = '';
-      calculateOperator = '';
+      FirstInput = "";
+      calculateOperator = "";
     }
-  } else if (value === 'ac') {
-
-    SecondInput = '';
-    FirstInput = '';
-    calculateOperator = '';
-  } else if (value === 'de') {
-
+  } else if (value === "ac") {
+    SecondInput = "";
+    FirstInput = "";
+    calculateOperator = "";
+  } else if (value === "de") {
     SecondInput = SecondInput.slice(0, -1);
-
-  } else if (['+', '-', '*', '/', '^', 'mod'].includes(value)) {
-
+  } else if (["+", "-", "*", "/", "^", "mod"].includes(value)) {
     if (SecondInput && FirstInput && calculateOperator) {
-
-      handleInput('=');
+      handleInput("=");
     }
     calculateOperator = value;
     FirstInput = SecondInput;
-    SecondInput = '';
+    SecondInput = "";
   } else {
-    
     SecondInput += value;
   }
   displayResult();
 }
 
-calculatorButtons.forEach(button => {
-  const value = button.getAttribute('value');
-  button.addEventListener('click', () => {
+calculatorButtons.forEach((button) => {
+  const value = button.getAttribute("value");
+  button.addEventListener("click", () => {
     handleInput(value);
   });
 });
