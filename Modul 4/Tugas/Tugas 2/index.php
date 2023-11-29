c8802358636874029d7265600245ad185aa96167
lsawfa <laryntsawfakenanga@gmail.com>
2023-11-29 21:12:47 +0700

    commit codelab modul 5

--- Modul 4/Tugas/Tugas 2/index.php
+++ Modul 4/Tugas/Tugas 2/index.php
@@ -4,8 +4,8 @@ include("Controllers/DramaController.php");
 use Controller\DramaController;
 
 $DramaController = new DramaController();
-echo $DramaController->getAllDrama();
+// echo $DramaController->getAllDrama();
 // echo $DramaController->getDrama(1);
 // echo $DramaController->getDrama(2);
 // echo $DramaController->getDrama(3);
-// echo $DramaController->getDrama(4);
+echo $DramaController->getDrama(4);
